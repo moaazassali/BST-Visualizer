0ea9786bbb78f546d7deb31f185645bf7762d311
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2020-12-22 03:34:14 +0400

    Initial Commit
    All project files and ReactJS build to host website on GitHub Pages

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import AVLTreeVisualizer from "./AVLTreeVisualizer/AVLTreeVisualizer.jsx"
+
+function App() {
+    return (
+        <AVLTreeVisualizer />
+    )
+}
+
+export default App
